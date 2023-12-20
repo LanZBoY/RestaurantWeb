2a89b4ab46ce7f33398c423ea33ff58428e4c69b
WenTee <superjimalex@gmail.com>
2023-12-20 19:25:58 +0800

    整合Mgmt功能到UserAPI中

--- Controllers/MgmtUserController.cs
+++ Controllers/MgmtUserController.cs
@@ -5,26 +5,17 @@ namespace Restaurant.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class MgmtUsersController(UserContext userContext) : ControllerBase{
+public class MgmtUserController(UserContext userContext) : ControllerBase{
     private readonly UserContext _context = userContext;
 
     [HttpGet]
     public ActionResult<List<MgmtViewUser>> GetAllUser(){
         return Ok(_context.MgmtViewUsers.ToList());
     }
-}
-
-[ApiController]
-[Route("[controller]")]
-public class MgmtUserController(UserContext userContext) : ControllerBase{
-    private readonly UserContext _context = userContext;
 
     [HttpGet("{UserName}")]
-    public ActionResult<MgmtViewUser> GetUser(string UserName){
-        MgmtViewUser? result = _context.MgmtViewUsers.Where((item) => item.UserName == UserName).FirstOrDefault();
-        if (result == null){
-            return NotFound();
-        }
+    public ActionResult<List<MgmtViewUser>> FindUser(string UserName){
+        List<MgmtViewUser>? result = [.. _context.MgmtViewUsers.Where((item) => item.UserName == UserName)];
         return Ok(result);
     }
 }
